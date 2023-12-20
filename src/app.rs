d721356d4a41a516972a80812f87ff47c7f1d620
David Åsberg <david.aasberg@gmail.com>
2023-12-20 16:01:42 +0100

    First page test

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,31 @@
 use yew::prelude::*;
 
-#[function_component]
-pub fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
+#[function_component(App)]
+pub fn app() -> Html {
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+        <div class="flex flex-row min-h-screen justify-center items-center m-4">
+        <div class="flex flex-col items-center gap-8 w-3/4">
+            <ClassCard />
+            <ClassCard />
+            <ClassCard />
+            <ClassCard />
+        </div>
+        </div>
+    }
+}
+
+#[function_component(ClassCard)]
+fn class_card() -> Html {
+    html! {
+        <div class="card-side flex flex-row shadow-xl bg-neutral">
+            <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
+            <div class="card-body">
+                <h2 class="card-title">{"Fighter"}</h2>
+                <p>{"Fight your way through anything"}</p>
+                <div class="card-actions justify-end">
+                <button class="btn btn-primary">{"Read more"}</button>
+                </div>
+            </div>
         </div>
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,32 @@ module.exports = {
 	plugins: [
 		// require('@tailwindcss/typography'),
 		// require('@tailwindcss/forms'),
+		require("daisyui")
 	],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+
+					"primary": "#facc15",
+
+					"secondary": "#f43f5e",
+
+					"accent": "#0064ff",
+
+					"neutral": "#0f0f0f",
+
+					"base-100": "#222222",
+
+					"info": "#00adcb",
+
+					"success": "#548100",
+
+					"warning": "#ea7e00",
+
+					"error": "#e6364a",
+				},
+			},
+		],
+	},
 };
