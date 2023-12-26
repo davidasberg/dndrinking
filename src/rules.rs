44fdfa1e9f77b49c21bf97317af0f35e24fdab48
David Åsberg <david.aasberg@gmail.com>
2023-12-26 14:54:18 +0100

    improve rules page

--- src/rules.rs
+++ src/rules.rs
@@ -3,34 +3,34 @@ use yew::prelude::*;
 #[function_component(Rules)]
 pub fn rules() -> Html {
     html! {
-        <div class="container mx-auto flex flex-col my-10 px-4 sm:px-6 lg:px-8 items-center">
-            <h1 class="text-4xl font-extrabold tracking-tight text-center sm:text-5xl lg:text-6xl">{"Rules"}</h1>
+        <div class="container mx-auto flex flex-col my-10 px-4 sm:px-6 lg:px-8 items-center justify-items-center">
+            <h1 class="text-4xl font-extrabold text-center sm:text-5xl lg:text-6xl">{"Rules"}</h1>
 
-            <p class="text-lg font-bold tracking-tight text-center w-1/2">{"Spelet spelas med en D20. Alla börjar med att slå tärningen en gång. Personen som slår högst får välja klass först. Vid slag av 20 får man även välja DM. Om två personer slår samma får de tävla i sten sax påse om vem som får välja först."}</p>
+            <p class="text-lg font-bold text-center tracking-tight text-balance">{"Spelet spelas med en D20. Alla börjar med att slå tärningen en gång. Personen som slår högst får välja klass först. Vid slag av 20 får man även välja DM. Om två personer slår samma får de tävla i sten sax påse om vem som får välja först."}</p>
 
-            <div class="divider self-center w-1/2"></div>
+            <div class="divider"></div>
 
-            <ol class="list-decimal list-inside space-y-4 w-1/2">
-                <li class="text-lg">{"Critical Failure! Ta en shot!"}</li>
-                <li class="text-lg">{"Svep resten!"}</li>
-                <li class="text-lg">{"Slå och drick"}</li>
-                <li class="text-lg">{"Four is whore"}</li>
-                <li class="text-lg">{"Alla dricker"}</li>
-                <li class="text-lg">{"Six is dicks"}</li>
-                <li class="text-lg">{"Slå igen"}</li>
-                <li class="text-lg">{"Räkna till 3. På 3 håller alla upp ett nummer 1-5. Drick så många klunkar som antal personer som väljer samma nummer. Är du själv, dricker du inget!"}</li>
-                <li class="text-lg">{"Ta två klunkar"}</li>
-                <li class="text-lg">{"Ta ett ölkort! Ge det till någon annan."}</li>
-                <li class="text-lg">{"Alla byter plats"}</li>
-                <li class="text-lg">{"Slå igen. Vid jämt slag dricker personen till höger 2 klunkar, vid udda dricker personen till vänster 2 klunkar."}</li>
-                <li class="text-lg">{"Vänta på nästa spelare. Både du och nästa spelare får det tärningslaget."}</li>
-                <li class="text-lg">{"Slå igen"}</li>
-                <li class="text-lg">{"Question Master"}</li>
-                <li class="text-lg">{"Alla andra dricker en klunk."}</li>
-                <li class="text-lg">{"Slå och ge"}</li>
-                <li class="text-lg">{"Välj en kompis! Kompisen måste dricka med dig"}</li>
-                <li class="text-lg">{"Ge en shot!"}</li>
-                <li class="text-lg">{"Ta ett ölkort!"}</li>
+            <ol class="list-decimal list-inside font-bold space-y-4">
+                <li>{"Critical Failure! Ta en shot!"}</li>
+                <li>{"Svep resten!"}</li>
+                <li>{"Slå och drick"}</li>
+                <li>{"Four is whore"}</li>
+                <li>{"Alla dricker"}</li>
+                <li>{"Six is dicks"}</li>
+                <li>{"Slå igen"}</li>
+                <li>{"Räkna till 3. På 3 håller alla upp ett nummer 1-5. Drick så många klunkar som antal personer som väljer samma nummer. Är du själv, dricker du inget!"}</li>
+                <li>{"Ta två klunkar"}</li>
+                <li>{"Ta ett ölkort! Ge det till någon annan."}</li>
+                <li>{"Alla byter plats"}</li>
+                <li>{"Slå igen. Vid jämt slag dricker personen till höger 2 klunkar, vid udda dricker personen till vänster 2 klunkar."}</li>
+                <li>{"Vänta på nästa spelare. Både du och nästa spelare får det tärningslaget."}</li>
+                <li>{"Slå igen"}</li>
+                <li>{"Question Master"}</li>
+                <li>{"Alla andra dricker en klunk."}</li>
+                <li>{"Slå och ge"}</li>
+                <li>{"Välj en kompis! Kompisen måste dricka med dig"}</li>
+                <li>{"Ge en shot!"}</li>
+                <li>{"Ta ett ölkort!"}</li>
             </ol>
 
         </div>
